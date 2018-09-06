ecc4e46aa3fcaeabf3a8106730241bfb9721336e
bbarres <benoit.barres@gmail.com>
2018-09-07 01:12:18 +0200

    adding maps for the AFPP meeting

---
+++ cartes_afpp.R
@@ -0,0 +1,52 @@
+###############################################################################
+###############################################################################
+#Mapping the different resistance for AFPP meeting
+###############################################################################
+###############################################################################
+
+#loading the packages necessary for the analysis
+library(rgdal)
+library(plotrix)
+library(classInt)
+library(mapplots)
+
+#Setting the right working directory
+setwd("~/work/Rfichiers/Githuber/mildiou_mito_comp_data")
+
+
+###############################################################################
+#loading the geographical data
+###############################################################################
+
+#Setting the right working directory
+setwd("~/work/Rfichiers/Githuber/carto_france")
+
+#the geographical layer used here were downloaded on the IGN website (Geofla), 
+#which is the French Institut of Geographic and forest information
+
+#the smallest administrative unit in France: communes
+commu<-readOGR(dsn="C:/Users/Benoit/Documents/Work/Rfichiers/Githuber/geo_data/ADE_1-1_SHP_LAMB93_FR",
+               layer="COMMUNE")
+class(commu)
+slotNames(commu)
+summary(commu@data)
+
+#arrondissement are an intermediate
+arrond<-readOGR(dsn="C:/Users/Benoit/Documents/Work/Rfichiers/Githuber/geo_data/ADE_1-1_SHP_LAMB93_FR",
+                layer="ARRONDISSEMENT_DEPARTEMENTAL")
+
+departe<-readOGR(dsn="C:/Users/Benoit/Documents/Work/Rfichiers/Githuber/geo_data/ADE_1-1_SHP_LAMB93_FR",
+                 layer="DEPARTEMENT")
+
+regions<-readOGR(dsn="C:/Users/Benoit/Documents/Work/Rfichiers/Githuber/geo_data/ADE_1-1_SHP_LAMB93_FR",
+                 layer="REGION")
+
+#isolate the information in the spatial data on the communes
+db_commu<-commu@data
+summary(db_commu)
+db_arrond<-arrond@data
+db_arrond$DEPARR<-paste(db_arrond$INSEE_DEP,db_arrond$INSEE_ARR)
+
+#back to the data folder
+setwd("~/work/Rfichiers/Githuber/mildiou_mito_comp_data")
+
